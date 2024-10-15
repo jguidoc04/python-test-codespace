fe15b7b432b775998cc2dd1142069d03397aca5c
jguidoc04 <jguidoc04@gmail.com>
2024-10-15 22:07:18 +0000

    Pending changes exported from your codespace

--- main.py
+++ main.py
@@ -1,1 +1,17 @@
-print("Test")
+cadena = "Hola123Mundo"
+
+print(cadena.isalnum())
+
+
+
+hay_numeros = False
+
+for caracter in cadena:
+    if caracter.isdigit():
+        hay_numeros = True
+        break
+
+if hay_numeros:
+    print("La cadena contiene números.")
+else:
+    print("La cadena no contiene números.")
